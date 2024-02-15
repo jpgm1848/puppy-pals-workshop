4ec92e0a0e4b0a4fc5d418647c423aea531104c3
jpgm1848 <154918928+jpgm1848@users.noreply.github.com>
2024-02-15 17:11:19 -0400

    finished project

--- puppy-pals-workshop/src/App.jsx
+++ puppy-pals-workshop/src/App.jsx
@@ -1,16 +1,39 @@
 import "./App.css";
 import { puppyList } from "./data.js";
 import { useState } from "react";
+import "./App.css";
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-  console.log(puppies);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
   return (
     <>
+      <h1 id="header">Our Puppies</h1>
       <div className="App">
         {puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>;
+          return (
+            <p
+              className="indPup"
+              onClick={() => {
+                setFeatPupId(puppy.id);
+              }}
+              key={puppy.id}
+            >
+              {puppy.name}
+            </p>
+          );
         })}
+        {featPupId && (
+          <div className="FeaturedPuppy">
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
       </div>
     </>
   );
